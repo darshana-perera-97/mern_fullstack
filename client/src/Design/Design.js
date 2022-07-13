ba7f01180055c7140990c1639ff0b9180a50a933
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-13 22:45:02 +0530

    inputs created

--- client/src/Design/Design.js
+++ client/src/Design/Design.js
@@ -1,18 +1,31 @@
 import React from "react";
 
+import Axios from "axios";
+
 export default function Design() {
-  const [list, setList] = React.useState([
-    { id: 1, name: "dsp", age: 12, username: "pspp" },
-  ]);
+  const [list, setList] = React.useState([]);
+  React.useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((responce) => {
+      setList(responce.data);
+    });
+  }, []);
   return (
     <div>
       {list.map((user) => {
-        return <div>
+        return (
+          <div>
             <h1>Name :{user.name}</h1>
             <h1>Age :{user.age}</h1>
             <h1>UserName :{user.username}</h1>
-        </div>;
+          </div>
+        );
       })}
+      <div>
+        <input type="text" placeholder="name"/>
+        <input type="text" placeholder="age"/>
+        <input type="text" placeholder="username"/>
+        <button>Create user</button>
+      </div>
     </div>
   );
 }
