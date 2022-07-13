f27873af0a30bb6743618e0073e4612d299892de
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-13 19:30:33 +0530

    get request created for /getUseres

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
+const UserModel = require("./models/Users");
 const app = express();
 
-mongoose.connect("mongodb+srv://darshana:darshana@cluster0.b4ir7g9.mongodb.net/merntutorial?retryWrites=true&w=majority")
+mongoose.connect(
+  "mongodb+srv://darshana:darshana@cluster0.b4ir7g9.mongodb.net/merntutorial?retryWrites=true&w=majority"
+);
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
 
 app.listen(3001, () => {
   console.log("Server runs in 3001");
