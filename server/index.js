4ebf567b1bfa959930232685a3d596e1835a424f
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-13 19:52:43 +0530

    post request created for /createUser

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const { default: mongoose } = require("mongoose");
 const UserModel = require("./models/Users");
 const app = express();
 
+app.use(express.json());
+
 mongoose.connect(
   "mongodb+srv://darshana:darshana@cluster0.b4ir7g9.mongodb.net/merntutorial?retryWrites=true&w=majority"
 );
@@ -15,6 +17,14 @@ app.get("/getUsers", (req, res) => {
       res.json(result);
     }
   });
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
 });
 
 app.listen(3001, () => {
