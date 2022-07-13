c70fa0f43bf38aaf1055314ed2764fedac1bbdc7
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-13 22:39:35 +0530

    test react app with hardcoded data

--- client/src/Design/Design.js
+++ client/src/Design/Design.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Design() {
+  const [list, setList] = React.useState([
+    { id: 1, name: "dsp", age: 12, username: "pspp" },
+  ]);
   return (
-    <div>Design</div>
-  )
+    <div>
+      {list.map((user) => {
+        return <div>
+            <h1>Name :{user.name}</h1>
+            <h1>Age :{user.age}</h1>
+            <h1>UserName :{user.username}</h1>
+        </div>;
+      })}
+    </div>
+  );
 }
